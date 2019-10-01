293d3b96e03f2a346ccf16eb14029aa426933f1a
RemainTheSame <halooscur.orava@gmail.com>
2019-10-01 21:28:29 +0200

    BlogList prepared, name change for test

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -22,62 +22,18 @@ class BlogList extends React.Component{
 
     //Generate random dog picture and advice
     componentDidMount() {
-        axios.get("https://dog.ceo/api/breeds/image/random")
-            .then(response =>{
-                this.setState({
-                    "image":response.data.message
-                });
-                console.log(response.data)
-            })
-            .catch(error =>{
-                console.log(error);
-            })
 
-        axios.get("https://api.adviceslip.com/advice")
-            .then(response =>{
-                this.setState({
-                    "text":response.data.slip.advice
-                });
-                console.log(response.data)
-            })
-            .catch(error =>{
-                console.log(error);
-            })
     }
 
     buttonClick=()=>{
-        axios.get("https://dog.ceo/api/breeds/image/random")
-            .then(response =>{
-                this.setState({
-                    "image":response.data.message
-                });
-                console.log(response.data)
-            })
-            .catch(error =>{
-                console.log(error);
-            })
 
-        axios.get("https://api.adviceslip.com/advice")
-            .then(response =>{
-                this.setState({
-                    "text":response.data.slip.advice
-                });
-                console.log(response.data)
-            })
-            .catch(error =>{
-                console.log(error);
-            })
     }
 
 
     render(){
         return(
             <div className={"column-div"}>
-                <div className={"fancy-text"}>
-                    {this.state.text}
-                </div>
-                <img width={500} height={400} src={this.state.image} alt={""}/>
-                <button onClick={this.buttonClick}>Click for New Advice and Doggo</button>
+
             </div>
         )
     }
